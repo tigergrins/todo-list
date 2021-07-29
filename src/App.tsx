7b1a326068b496d7189db349b9bb90902eaa74f9
Danila <d.bokun@yandex.ru>
2021-07-29 23:07:21 +0300

    lesson 8 (reducer, unit test)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import {Menu} from '@material-ui/icons';
 
 export type FilterValuesType = 'all' | 'active' | 'completed';
 
-type TodolistType = {
+export type TodolistType = {
     id: string
     title: string
     filter: FilterValuesType
